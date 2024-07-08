ee4de5f472fa3a6242746d4c85d759299aa28855
Raju <tnraju1910@gmail.com>
2024-07-08 18:18:57 +0530

    updated app.js

--- app.js
+++ app.js
@@ -12,20 +12,31 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_DB)
-  .then((e) => console.log("connected to MONGO_DB"))
-  .catch((e) => console.log(e));
+  .connect(process.env.MONGO_DB, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MONGO_DB"))
+  .catch((e) => {
+    console.error("Error connecting to MONGO_DB:", e);
+    process.exit(1); // Exit the process if there is a DB connection error
+  });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`server is runnig at port number ${PORT}`);
+  console.log(`Server is running at port number ${PORT}`);
 });
 
 app.use("/api", router);
 
 app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Welocome to my bomma backend" });
+  return res.status(200).json({ message: "Welcome to my bomma backend" });
 });
 
 app.options("/api/admin-dashboard", cors());
+
+// Error handling for unknown routes
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
